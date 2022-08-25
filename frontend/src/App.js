04d59272f2f96da73f3dcd6a151ea8de2054d770
calebbenjin <calebben94@gmail.com>
2022-08-25 08:29:41 +0100

    Ebuka added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,20 @@ import Login from './pages/admin/index';
 import Upload from './pages/admin/upload';
 import Dashboard from './pages/admin/dashboard';
 import Daalu from './pages/Daalu';
+import AnalyticsEventTracker from './components/AnalyticsEventTracker'
+import ReactGA from 'react-ga'
+
+
+
+
+
+const TRACKING_ID = "UA-12341234-1"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
     <>
+      
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route exact path='/home' element={<Home />} />
