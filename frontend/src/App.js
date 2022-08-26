7d3af7917cbd8a388c0e240ce4653423b0d4575a
calebbenjin <calebben94@gmail.com>
2022-08-26 10:35:27 +0100

    page prevei

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,15 @@ import Login from './pages/admin/index';
 import Upload from './pages/admin/upload';
 import Dashboard from './pages/admin/dashboard';
 import Daalu from './pages/Daalu';
-import AnalyticsEventTracker from './components/AnalyticsEventTracker'
-import ReactGA from 'react-ga'
+// import AnalyticsEventTracker from './components/AnalyticsEventTracker'
+// import ReactGA from 'react-ga'
 
 
 
 
 
-const TRACKING_ID = "UA-12341234-1"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = "UA-12341234-1";
+// ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
